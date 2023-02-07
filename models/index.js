234ad0b0a9d5f20fa9d35d99e22b3e3feed6e630
Adam Furmanek <adamf@metisdata.io>
2023-02-07 11:09:29 +0000

    Seeding database.

--- models/index.js
+++ models/index.js
@@ -31,6 +31,26 @@ Object.keys(db).forEach((modelName) => {
   }
 });
 
+db.seedDatabase = async () => {
+  sequelize.query(`DO $done$
+    BEGIN
+    
+    IF NOT EXISTS (
+        SELECT
+        FROM pg_catalog.pg_tables 
+        WHERE
+            schemaname = 'bookings'
+            AND tablename  = 'flights_indexed'
+    ) THEN
+      CREATE TABLE flights_indexed AS (SELECT * FROM flights);
+    
+      CREATE INDEX departure_aiport_idx ON flights_indexed(departure_airport);
+    END IF;
+    
+    END;
+    $done$`);
+};
+
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
