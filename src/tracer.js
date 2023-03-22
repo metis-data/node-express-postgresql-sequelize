2ba5ad744e292d039b71496f02abc5646e23f3dd
Adam Furmanek <adamf@metisdata.io>
2023-03-22 11:32:29 +0000

    Moving tracer to new thing! BEWARE

--- src/tracer.js
+++ src/tracer.js
@@ -1,14 +1,12 @@
 let opentelemetry = require('@opentelemetry/api');
 let { registerInstrumentations } = require('@opentelemetry/instrumentation');
-let { Resource } = require('@opentelemetry/resources');
 let {
   BasicTracerProvider,
   BatchSpanProcessor,
   ConsoleSpanExporter,
   SimpleSpanProcessor,
 } = require('@opentelemetry/sdk-trace-base');
-let { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-let { getMetisExporter, MetisHttpInstrumentation, MetisPgInstrumentation } = require('@metis-data/pg-interceptor');
+let { getMetisExporter, getResource, MetisHttpInstrumentation, MetisPgInstrumentation } = require('@metis-data/pg-interceptor');
 let { AsyncHooksContextManager } = require('@opentelemetry/context-async-hooks');
 
 let tracerProvider;
@@ -32,10 +30,7 @@ const connectionString = process.env.DATABASE_URL;
 
 const startMetisInstrumentation = () => {
   tracerProvider = new BasicTracerProvider({
-    resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: process.env.METIS_SERVICE_NAME,
-      [SemanticResourceAttributes.SERVICE_VERSION]: process.env.METIS_SERVICE_VERSION,
-    }),
+    resource: getResource(process.env.METIS_SERVICE_NAME, process.env.METIS_SERVICE_VERSION),
   });
 
   metisExporter = getMetisExporter(process.env.METIS_API_KEY);
