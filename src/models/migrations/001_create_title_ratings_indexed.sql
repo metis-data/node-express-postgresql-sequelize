114dde0cf7e409d9473110feed80b6601cbb3d4d
Adam Furmanek <adamf@metisdata.io>
2023-04-27 10:16:22 +0000

    Changing migration content

--- src/models/migrations/001_create_title_ratings_indexed.sql
+++ src/models/migrations/001_create_title_ratings_indexed.sql
@@ -1,16 +1,2 @@
-DO $done$
-  BEGIN
-  
-  IF NOT EXISTS (
-      SELECT
-      FROM pg_catalog.pg_tables 
-      WHERE
-          schemaname = 'imdb'
-          AND tablename  = 'title_ratings_indexed'
-  ) THEN
-    CREATE TABLE imdb.title_ratings_indexed AS (SELECT * FROM imdb.title_ratings);
-  
-    CREATE INDEX title_ratings_idx ON imdb.title_ratings_indexed(averagerating);
-  END IF;
-  END;
-$done$
+CREATE TABLE IF NOT EXISTS imdb.title_ratings_indexed AS (SELECT * FROM imdb.title_ratings);
+CREATE INDEX IF NOT EXISTS title_ratings_idx ON imdb.title_ratings_indexed(averagerating);
