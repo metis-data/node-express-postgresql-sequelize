c2e1eea1fa9b5a5ee1c42f263dd4b0e0aac35006
Adam Furmanek <adamf@metisdata.io>
2023-05-21 19:11:07 +0000

    Fixing cleanup script

--- remove-container.sh
+++ remove-container.sh
@@ -1,5 +1,5 @@
 ssh-keygen -R '[localhost]:58222'
-pkill -f "ssh -i $(pwd)/tunnel_rsa tunnel@localhost -p 58222"
+pkill -f "ssh -i $(pwd)/ssh_tunnel/tunnel_rsa tunnel@localhost -p 58222"
 docker stop node_express_postgresql_sequelize
 docker rm node_express_postgresql_sequelize
 docker rmi --force node_express_postgresql_sequelize
