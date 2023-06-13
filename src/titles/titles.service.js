6443baea78fafef4d8014c4cd088564034bc4761
Adam Furmanek <adamf@metisdata.io>
2023-06-13 14:23:37 +0000

    Improving query

--- src/titles/titles.service.js
+++ src/titles/titles.service.js
@@ -1,4 +1,5 @@
 const { Op } = require("sequelize");
+const sequelize = require('../models').sequelize;
 const titleBasic = require('../models').TitleBasic;
 const titlePrincipal = require('../models').TitlePrincipal;
 
@@ -26,6 +27,23 @@ module.exports = {
           ['startyear', 'DESC']
         ],
         limit: 10
-      })
+      });
+
+    /*  
+    return sequelize.query(`
+      SELECT TitleBasic.*
+      FROM imdb.title_basics AS TitleBasic
+      JOIN imdb.title_principals AS TitlePrincipals ON TitlePrincipals.tconst = TitleBasic.tconst
+      WHERE TitlePrincipals.nconst = :nconst
+      ORDER BY TitleBasic.startyear DESC
+      LIMIT 10
+    `, {
+      model: titleBasic,
+      mapToModel: true,
+      replacements: {
+        nconst: nconst
+      },
+    });
+    */
   }
 };
