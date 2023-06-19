f7f3057bb0dab21ad90cd5cef8f4cab80b605e11
Adam Furmanek <adamf@metisdata.io>
2023-06-19 11:17:42 +0000

    Removing SSH image

--- remove-container.sh
+++ remove-container.sh
@@ -5,6 +5,7 @@ docker rm node_express_postgresql_sequelize
 docker rmi --force node_express_postgresql_sequelize
 docker stop node_express_postgresql_sequelize_ssh
 docker rm node_express_postgresql_sequelize_ssh
+docker rmi node_express_postgresql_sequelize_ssh
 docker rmi lscr.io/linuxserver/openssh-server:latest
 docker system prune
 docker volume prune
