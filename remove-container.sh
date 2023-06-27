b06bd1c403bc2e616ae5d8249f4d4f6bbfd79a74
Adam Furmanek <adamf@metisdata.io>
2023-06-27 12:04:18 +0000

    Updating script to remove containers

--- remove-container.sh
+++ remove-container.sh
@@ -1,12 +1,12 @@
 ssh-keygen -R '[localhost]:58222'
 pkill -f "ssh -i $(pwd)/ssh_tunnel/tunnel_rsa tunnel@localhost -p 58222"
-docker stop node_express_postgresql_sequelize
-docker rm node_express_postgresql_sequelize
+docker stop node_express_postgresql_sequelize -t 1
+docker rm --force node_express_postgresql_sequelize
 docker rmi --force node_express_postgresql_sequelize
-docker stop node_express_postgresql_sequelize_ssh
-docker rm node_express_postgresql_sequelize_ssh
-docker rmi node_express_postgresql_sequelize_ssh
-docker rmi public.ecr.aws/o2c0x5x8/community-images-backup:lscr.io-linuxserver-openssh-server
-docker rmi public.ecr.aws/o2c0x5x8/community-images-backup:node-16-slim
+docker stop node_express_postgresql_sequelize_ssh -t 1
+docker rm --force node_express_postgresql_sequelize_ssh
+docker rmi --force node_express_postgresql_sequelize_ssh
+docker rmi --force public.ecr.aws/o2c0x5x8/community-images-backup:lscr.io-linuxserver-openssh-server
+docker rmi --force public.ecr.aws/o2c0x5x8/community-images-backup:node-16-slim
 docker system prune
 docker volume prune
