5644c06c8267870ad2ec0e5b547aaf82f3faa1f3
Adam Furmanek <adamf@metisdata.io>
2023-06-27 16:25:44 +0000

    Updating old API

--- src/titles/title_ratings.controller.js
+++ src/titles/title_ratings.controller.js
@@ -2,19 +2,21 @@ const titleRatingService = require('./title_ratings.service');
 
 module.exports = {
   initialize(router) {
-    router.get('/titles/ratings/best', this.getBestMovies);
-    router.get('/titles/ratingsIndexed/best', this.getBestMoviesIndexed);
+    router.get('/titles/ratings/best', this.handleResult(this.getBestMovies));
+    router.get('/titles/ratingsIndexed/best', this.handleResult(this.getBestMoviesIndexed));
   },
 
   getBestMovies(req, res) {
-    return titleRatingService.getBestMovies(req.params.airportCode)
-      .then((results) => res.status(200).send(results))
-      .catch((error) => { res.status(400).send(error); });
+    return titleRatingService.getBestMovies();
   },
 
   getBestMoviesIndexed(req, res) {
-    return titleRatingService.getBestMoviesIndexed(req.params.airportCode)
+    return titleRatingService.getBestMoviesIndexed();
+  },
+
+  handleResult(lambda) {
+    return (req, res) => lambda(req, res)
       .then((results) => res.status(200).send(results))
-      .catch((error) => { res.status(400).send(error); });
+      .catch((error) => { console.log(error); res.status(400).send(error); });
   }
 };
