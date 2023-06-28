f80b2351799f3588bcdaa505371ba4ac233219a9
Adam Furmanek <adamf@metisdata.io>
2023-06-28 17:51:43 +0000

    Testing all scenarios

--- src/titles/title_ratings.spec.js
+++ src/titles/title_ratings.spec.js
@@ -1,29 +1,39 @@
 require('dotenv').config();
-const { startMetisInstrumentation, shudownhook } = require('../tracer');
+const { startMetisInstrumentation } = require('../tracer');
 const request = require('supertest');
 
-describe('AppController (e2e)', () => {
-  let app;
+describe('AppController (e2e)', function (){
+  this.timeout(0);
 
-  before(async () => {
+  const endpoints = [
+    '/titles/ratings/best',
+    '/titles/ratingsIndexed/best',
+    '/titles?title=Test',
+    '/titlesForAnActor?nconst=nm1588970',
+    '/highestRatestMoviesForAnActor?nconst=nm1588970',
+    '/highestRatestMovies?numvotes=10000',
+    '/commonMoviesForTwoActors?actor1=nm0302368&actor2=nm0001908',
+    '/crewOfGivenMove?tconst=tt0000439',
+    '/mostProlificActorInPeriod?startYear=1900&endYear=1912',
+    '/mostProlificActorInGenre?genre=Action',
+    '/mostCommonTeammates?nconst=nm0000428',
+  ];
+
+  let app;
+  let models;
+
+  before(async function() {
+    this.timeout(0);
     startMetisInstrumentation();
     
+    models = require('../models');
+    await models.seedDatabase();
     app = require('../app');
   });
 
-  it('/titles/ratings/best (GET)', async () => {
+  endpoints.map(url => it(`${url} (GET)`, async function() {
     await request(app)
-      .get('/titles/ratings/best')
+      .get(url)
       .expect(200);
-  });
-
-  it('/titles/ratingsIndexed/best (GET)', async () => {
-    await request(app)
-      .get('/titles/ratingsIndexed/best')
-      .expect(200);
-  });
-
-  after(async () => {
-    await shudownhook();
-  });
+  }));
 });
