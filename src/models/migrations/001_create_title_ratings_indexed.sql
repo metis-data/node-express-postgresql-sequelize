3dbb8df9d95672a5bdb82aadb97bbe9718da71f4
Adam Furmanek <adamf@metisdata.io>
2024-01-19 10:04:44 +0000

    Commit with dot.

--- src/models/migrations/001_create_title_ratings_indexed.sql
+++ src/models/migrations/001_create_title_ratings_indexed.sql
@@ -1,2 +1,5 @@
 CREATE TABLE IF NOT EXISTS imdb.title_ratings_indexed AS (SELECT * FROM imdb.title_ratings);
 CREATE INDEX IF NOT EXISTS title_ratings_idx ON imdb.title_ratings_indexed(averagerating);
+
+CREATE TABLE IF NOT EXISTS imdb.title_ratings_indexed2 AS (SELECT * FROM imdb.title_ratings);
+CREATE INDEX IF NOT EXISTS title_ratings_idx2 ON imdb.title_ratings_indexed(averagerating);
