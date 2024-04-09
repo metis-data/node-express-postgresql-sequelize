c097665eee90a4409cff65ab25840b814209f477
Adam Furmanek <adamf@metisdata.io>
2024-04-09 13:19:04 +0000

    Changes with dot.

---
+++ src/models/migrations/002_create_title_ratings_indexed.sql
@@ -0,0 +1,2 @@
+CREATE TABLE IF NOT EXISTS imdb.title_ratings_indexed2 AS (SELECT * FROM imdb.title_ratings);
+CREATE INDEX IF NOT EXISTS title_ratings_idx2 ON imdb.title_ratings_indexed2(averagerating);
